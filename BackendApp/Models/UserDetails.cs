b183caa3a8f498ead82721fba9547cda666eae5e
natalia.bala <natalia.bala@microsoft.wsei.edu.pl>
2025-04-29 17:23:28 +0200

    test

--- BackendApp/Models/UserDetails.cs
+++ BackendApp/Models/UserDetails.cs
@@ -1,6 +1,13 @@
-namespace BackendApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class UserDetails
+namespace ApplicationCore.Models
 {
-    public DateTime CreatedAt { get; set; }
+    public class UserDetails
+    {
+        public DateTime CreatedAt { get; set; }
+    }
 }
